ff0639a3c3e6810ae6ddc097815cd4c9465486a7
saurab86 <karki.saurab18@gmail.com>
2022-06-23 15:19:15 +0545

    commit

--- 3_module.js
+++ 3_module.js
@@ -1,5 +1,8 @@
 // Module
+// In CommonJS, every file is module (by default)
+// Modules - Encaspsulated Code(only share minunum)
 
+const secret = 'SUPER SECRET'
 const susan = "Susan"
 const peter = "peter"
 const sayHi = (name)=>{
