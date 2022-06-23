da40c9c7aae86c2879ff4d5bcc28d726225eeed5
saurab86 <karki.saurab18@gmail.com>
2022-06-23 15:11:49 +0545

    module.js

--- 2_intro.js
+++ 2_intro.js
@@ -5,3 +5,8 @@
 // require     - function to use modules(CommonJS)
 // module       - info about current module(file)
 // process     - info about env where the program is being executed  
+
+console.log(__dirname)
+setInterval(()=>{
+    console.log('This setInnterval which repeat the process until user interference with interval of 100ms')
+},1000)
---
+++ 3_module.js
@@ -0,0 +1,11 @@
+// Module
+
+const susan = "Susan"
+const peter = "peter"
+const sayHi = (name)=>{
+    console.log(`Hello there ${name}`)
+}
+
+sayHi('John Snow')
+sayHi(susan)
+sayHi(peter)
