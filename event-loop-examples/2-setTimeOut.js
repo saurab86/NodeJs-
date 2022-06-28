d58edd218782221d8e8b2d50a94ab6976d35d959
saurab86 <karki.saurab18@gmail.com>
2022-06-28 15:22:37 +0545

    setTimeOut eventloop

---
+++ event-loop-examples/2-setTimeOut.js
@@ -0,0 +1,9 @@
+// started operating system process
+
+console.log('first')
+
+setTimeout(()=>{
+    console.log('second')
+})
+console.log('third')
+// completed and exited operating system process
